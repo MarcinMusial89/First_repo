c3413984d954748586b7dd003b19f86d3ee555e0
MarcinMusial89 <marcinmusial89@gmail.com>
2023-02-11 12:55:58 +0100

    'update_12_55_11.02.2023'

--- TDD_test.py
+++ TDD_test.py
@@ -1,15 +1,38 @@
+# import functions
+#
+# print(functions.multiply(5, 6))
+#
+# def test_multiplay():
+#     assert functions.multiply(5, 10) == 50 #first test
+#     assert functions.multiply(100, 1.1) == 110
+#     assert functions.multiply(1.5, 1.5) == 2.25
+#     assert functions.multiply(0.0000001, 100) == 0.00001
+#     assert functions.multiply(None, 5) == None
+#
+# def test_number_of_letter():
+#     assert functions.no_of_letter('mama') == 4
+#     assert functions.no_of_letter('mama.tata') == 8
+#     #nie odpowiedzielimsy na ten warunek
 import functions
 
-print(functions.multiply(5, 6))
+# def test_fissbuzz():
+#     assert functions.fissbuzz(1) == 1
+#     assert functions.fissbuzz(2) == 2
+#     assert functions.fissbuzz(3) == 'fiss'
+#     assert functions.fissbuzz(4) == 4
+#     assert functions.fissbuzz(5) == 'buzz'
+#     assert functions.fissbuzz(6) == 'fiss'
+#     assert functions.fissbuzz(10) == 'buzz'
 
-def test_multiplay():
-    assert functions.multiply(5, 10) == 50 #first test
-    assert functions.multiply(100, 1.1) == 110
-    assert functions.multiply(1.5, 1.5) == 2.25
-    assert functions.multiply(0.0000001, 100) == 0.00001
-    assert functions.multiply(None, 5) == None
-
-def test_number_of_letter():
-    assert functions.no_of_letter('mama') == 4
-    assert functions.no_of_letter('mama.tata') == 8
-    #nie odpowiedzielimsy na ten warunek
+def test_fissbuzz_advanced():
+    assert functions.fissbuzz(1.3) == 1
+    assert functions.fissbuzz(1.9) == 1
+    assert functions.fissbuzz('1') == 1
+    assert functions.fissbuzz('1.7') == 1
+    assert functions.fissbuzz(5.9) == 'buzz'
+    assert functions.fissbuzz('5.99') == 'buzz'
+    assert functions.fissbuzz(0) == None
+    assert functions.fissbuzz(0.999) == None
+    assert functions.fissbuzz('mama') == None
+    assert functions.fissbuzz('-5') == None
+    assert functions.fissbuzz('-15.8') == None
--- functions.py
+++ functions.py
@@ -1,9 +1,26 @@
-def multiply(x, y):
-    if isinstance(x, (int, float)) and isinstance(y,  (int, float)):
-        return round(x * y, 5)
+# def multiply(x, y):
+#     if isinstance(x, (int, float)) and isinstance(y,  (int, float)):
+#         return round(x * y, 5)
+#     else:
+#         return round(x * y)
+#
+#
+# def no_of_letter(text):
+#     return len(text)
+
+def fissbuzz(input):
+    try:
+        input = int(float(input))
+    except:
+        input = 0
+
+    if input <= 0:
+        return None
+    elif input % 3 == 0:
+        return ('fiss')
+    elif input % 5 == 0:
+        return ('buzz')
+    elif input % 3 == 0 and input % 5 == 0:
+        return ('fissBuss')
     else:
-        return round(x * y)
-
-
-def no_of_letter(text):
-    return len(text)
+        return input
